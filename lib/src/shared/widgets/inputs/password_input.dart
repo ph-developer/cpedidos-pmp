9996765c15c66367b44709289a64533fa6f4aba6
ph <raphaelcr.ads@gmail.com>
2023-02-21 12:15:53 -0300

    feat(widgets): added showPassword functionality to PasswordInput.

--- lib/src/shared/widgets/inputs/password_input.dart
+++ lib/src/shared/widgets/inputs/password_input.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class PasswordInput extends StatelessWidget {
+class PasswordInput extends StatefulWidget {
   final bool isEnabled;
   final TextEditingController controller;
   final String label;
@@ -15,23 +15,40 @@ class PasswordInput extends StatelessWidget {
   });
 
   @override
+  State<PasswordInput> createState() => _PasswordInputState();
+}
+
+class _PasswordInputState extends State<PasswordInput> {
+  var showPassword = false;
+
+  @override
   Widget build(BuildContext context) {
     return TextField(
-      enabled: isEnabled,
-      controller: controller,
-      onSubmitted: onSubmitted,
-      obscureText: true,
+      enabled: widget.isEnabled,
+      controller: widget.controller,
+      onSubmitted: widget.onSubmitted,
+      obscureText: !showPassword,
       enableSuggestions: false,
       autocorrect: false,
       style: TextStyle(
-        color: isEnabled ? null : Theme.of(context).disabledColor,
+        color: widget.isEnabled ? null : Theme.of(context).disabledColor,
       ),
       decoration: InputDecoration(
-        label: Text(label),
+        label: Text(widget.label),
         border: const OutlineInputBorder(),
         floatingLabelBehavior: FloatingLabelBehavior.always,
         isDense: true,
-        enabled: isEnabled,
+        enabled: widget.isEnabled,
+        suffixIcon: IconButton(
+          icon: Icon(showPassword
+              ? Icons.lock_open_rounded
+              : Icons.lock_outline_rounded),
+          onPressed: () {
+            setState(() {
+              showPassword = !showPassword;
+            });
+          },
+        ),
       ),
     );
   }
