fff2a3e841e349e095a024bcdf19902c27aea1f0
ph <raphaelcr.ads@gmail.com>
2023-02-26 16:32:32 -0300

    chore(auth): removed unused failure type.

--- lib/src/auth/domain/errors/failures.dart
+++ lib/src/auth/domain/errors/failures.dart
@@ -7,11 +7,6 @@ class InvalidInput extends AuthFailure {
   const InvalidInput(super.message);
 }
 
-class UserDataNotFound extends AuthFailure {
-  const UserDataNotFound()
-      : super('Os dados do usuário não foram encontrados no banco de dados.');
-}
-
 class InvalidCredentials extends AuthFailure {
   const InvalidCredentials() : super('Credenciais inválidas.');
 }
