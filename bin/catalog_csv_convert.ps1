5039aaab6029fb9922aa5547ab47ddd3f2f067cd
ph <raphaelcr.ads@gmail.com>
2023-03-20 11:03:14 -0300

    chore: removed catalog converter scripts and binaries

--- bin/catalog_csv_convert.ps1
+++
@@ -1,4 +0,0 @@
-.\bin\catalog_csv_converter.exe catser -i .\assets\catalog\catser.csv -o .\assets\catalog\catser.json
-.\bin\catalog_csv_converter.exe catser -i .\assets\catalog\catser.csv -o .\assets\catalog\catser.min.json -m
-.\bin\catalog_csv_converter.exe catmat -i .\assets\catalog\catmat.csv -o .\assets\catalog\catmat.json
-.\bin\catalog_csv_converter.exe catmat -i .\assets\catalog\catmat.csv -o .\assets\catalog\catmat.min.json -m
